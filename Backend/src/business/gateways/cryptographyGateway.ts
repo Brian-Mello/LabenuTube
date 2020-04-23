34e88e7e366a121ef08ad5577c5cfa04e1bf3556
Brian de Paula Mello <35143307+Brian-Mello@users.noreply.github.com>
2020-04-23 12:56:31 -0300

    Back end retirado

--- Backend/src/business/gateways/cryptographyGateway.ts
+++
@@ -1,4 +0,0 @@
-export interface CryptographyGateway {
-    encrypt(word: string): Promise<string>;
-    compare(word: string, hash: string): Promise<boolean>;
-}
--- Backend/src/business/gateways/userGateway.ts
+++
@@ -1,9 +0,0 @@
-import { User } from "../entites/user";
-
-export interface UserGateway {
-    signUp(user: User): Promise<void>;
-    login(email: string): Promise<User | undefined>;
-    getAllUsers(): Promise<User[] | undefined>;
-    getUserById(id: string): Promise<User | undefined>;
-    updatePassword(id: string, password: string): Promise<void>;
-}
